4d95ac78b6be2b2858261c1aa2a4d70a57609be2
akarib <alaeddinekarib@gmail.com>
2018-10-30 00:13:00 +0100

    def2

--- IdeaProjects/Dev/src/controller/Emp.py
+++ IdeaProjects/Dev/src/controller/Emp.py
@@ -8,6 +8,9 @@ class Employee:
         self.last = last
         self.full = first + '.' + last+"@company.com"
 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
 emp_1 = Employee('krb', 'Ala')
 
 #emp_2 = Employee()
@@ -19,5 +22,5 @@ emp_1 = Employee('krb', 'Ala')
 #emp_1.full = emp_1.firstfirst +' '+emp_1.last
 
 
-print(emp_1.full)
+print(emp_1.fullname())
 
